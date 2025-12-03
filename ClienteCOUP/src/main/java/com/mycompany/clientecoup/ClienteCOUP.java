39c301c44c7bbff90934577837489821ba4bba4e
MangoSalazar <l22440193@mochis.tecnm.mx>
2025-12-03 07:25:57 -0700

    Primer Commitm Cliente

---
+++ ClienteCOUP/src/main/java/com/mycompany/clientecoup/ClienteCOUP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clientecoup;
+
+/**
+ *
+ * @author M1-MQ1-D
+ */
+public class ClienteCOUP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
